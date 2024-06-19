69f511152a3917e1b571c08ae676829f1645dbf7
snimu <sebastian.nicolas.mueller@gmail.com>
2024-06-19 19:00:02 +0200

    Prepare new experiments (experiments_two.sh)

---
+++ experiments_two.sh
@@ -0,0 +1,5 @@
+python main.py -cw --logfile results_two.csv --wandb_project ul2.two --seed 1100
+python main.py -cw --append --logfile results_two.csv --wandb_project ul2.two --seed 1100 --ul2 --causal_divider 1.0 1.0 2.0 4.0 --s_denoiser 1.0 6.0 6.0 4.0 --r_denoiser 1.0 6.0 6.0 4.0 --x_denoiser 1.0 6.0 6.0 4.0 --causal_denoisers
+python main.py -cw --append --logfile results_two.csv --wandb_project ul2.two --seed 1100 --ul2 --causal_divider 1.0 1.0 2.0 4.0 --s_denoiser 1.0 6.0 6.0 4.0 --r_denoiser 1.0 6.0 6.0 4.0 --x_denoiser 1.0 6.0 6.0 4.0 --causal_denoisers --randomize_denoiser_settings
+python main.py -cw --append --logfile results_two.csv --wandb_project ul2.two --seed 1100 --ul2 --causal_divider 1.0 1.0 2.0 4.0 --s_denoiser 1.0 6.0 6.0 4.0 --r_denoiser 1.0 6.0 6.0 4.0 --x_denoiser 1.0 6.0 6.0 4.0
+python main.py -cw --append --logfile results_two.csv --wandb_project ul2.two --seed 1100 --ul2 --causal_divider 1.0 1.0 2.0 4.0 --s_denoiser 1.0 6.0 6.0 4.0 --r_denoiser 1.0 6.0 6.0 4.0 --x_denoiser 1.0 6.0 6.0 4.0 --randomize_denoiser_settings
