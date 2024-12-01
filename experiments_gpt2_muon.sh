49f627ffaedb5a8d909bb90fa6e49675ed6401ea
snimu <sebastian.nicolas.mueller@gmail.com>
2024-12-01 12:42:02 +0100

    better settings for the large experiments (after the previous one had a failed gpu

--- experiments_gpt2_muon.sh
+++ experiments_gpt2_muon.sh
@@ -1,5 +1,5 @@
 torchrun --nproc_per_node=8 train_gpt2_muon.py --input-bin="../edu_fineweb100B/edu_fineweb_train*" --input-val-bin="../edu_fineweb100B/edu_fineweb_val*" --wandb-project="ul2.gpt2.muon" --save-every=10000 --hf-repo="" --use-mask
 torchrun --nproc_per_node=8 train_gpt2_muon.py --input-bin="../edu_fineweb100B/edu_fineweb_train*" --input-val-bin="../edu_fineweb100B/edu_fineweb_val*" --wandb-project="ul2.gpt2.muon" --save-every=10000 --hf-repo=""
 
-torchrun --nproc_per_node=8 train_gpt2_muon.py --wandb-project="ul2.gpt2.muon.large" --n-layer=64 --n-embd=1792 --n-head=14 --device-batch-size=6 --warmdown-iters=15000 --seed=1234 --val-loss-every=650 --save-every=50000 --hf-repo="" --use-mask
-torchrun --nproc_per_node=8 train_gpt2_muon.py --wandb-project="ul2.gpt2.muon.large" --n-layer=64 --n-embd=1792 --n-head=14 --device-batch-size=6 --warmdown-iters=15000 --seed=1234 --val-loss-every=650 --save-every=50000 --hf-repo=""
+torchrun --nproc_per_node=8 train_gpt2_muon.py --wandb-project="ul2.gpt2.muon.large" --n-layer=64 --n-embd=1792 --n-head=14 --device-batch-size=6 --warmdown-iters=20000 --seed=1234 --val-loss-every=650 --save-every=50000 --hf-repo="" --use-mask
+torchrun --nproc_per_node=8 train_gpt2_muon.py --wandb-project="ul2.gpt2.muon.large" --n-layer=64 --n-embd=1792 --n-head=14 --device-batch-size=6 --warmdown-iters=20000 --seed=1234 --val-loss-every=650 --save-every=50000 --hf-repo=""
